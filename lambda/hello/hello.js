e127d1020ff41c2caafc0dcc1774d82ec5f98bf8
Randy Lai <randy.cs.lai@gmail.com>
2020-06-21 21:22:15 -0700

    .

--- lambda/hello/hello.js
+++ lambda/hello/hello.js
@@ -1,14 +1,12 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    console.log(context)
+    console.log(event.body)
+    console.log(event.queryStringParameters)
     const subject = event.queryStringParameters.name || 'World'
     return {
       statusCode: 200,
-      body: JSON.stringify(event) + JSON.stringify(context),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: event.body
     }
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
